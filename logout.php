885bb9ebd75edb7b42d0214e7651c52f1fb61249
Srijeeta <srijeeta.ghosh@innoraft.com>
2017-05-17 16:21:23 +0530

    Made changes in logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
+$server = $_SERVER['SERVER_NAME'];
+// echo $server;
 unset($_SESSION["userID"]);
 session_destroy();
-header('Location: http://radio.com/index.html')
+header('Location: http://'.$server.'/index.html');
 ?>
